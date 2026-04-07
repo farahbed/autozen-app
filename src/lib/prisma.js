8fc82bfcb48e871a0d5ddfa0d902dd40db3ff3c4
farahbed <farah.bendriss.la@gmail.com>
2026-04-07 14:30:07 +0200

    modified prisma file

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,5 @@
-import prisma from '@/lib/prisma'; // car lib est maintenant dans src
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export default prisma
+export default prisma;
